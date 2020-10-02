de0ef555d24ee601e762e5d8907bf14f148178c7
bazzl1202 <bazzl1202@wrdsb.ca>
2020-10-02 13:09:49 +0000

    Did loop examples

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * While loop example
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -9,7 +10,59 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create scanner for user input
+    Scanner input = new Scanner(System.in);
     
+    // declare the variable
+    int upTo;
+    do{
+      System.out.println("What number to count to (positive number)?");
+      upTo = input.nextInt();
+    }while(upTo <= 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // create a variable to keep track of the number 
+    int count = 1;
+
+   // using a loop to count up to five
+    while( count <= upTo ){
+      // print the number we are at
+      System.out.println(count);
+      // increase the value of count by 1
+      count = count + 1;
+    }
+    System.out.println("All done");
+
+    
+    //do while loop
+    int countTwo = 1;
+    //do while loop to count
+    do{
+      // print the number
+      System.out.println(countTwo);
+      // increment the counter
+      countTwo = countTwo + 1;
+    }while(countTwo <= upTo);
+    System.out.println("All done again");
+
+
+
   }
 }
